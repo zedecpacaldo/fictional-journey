97fa8336d7ffe8015b62c85fd70ba9e74c3baadd
Zedec Pacaldo <zedec.pacaldo@gmail.com>
2019-07-14 18:56:15 +0800

    Add files via upload

---
+++ Pre-Freshmen Year/main.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    double f, c;
+
+    printf("Enter the temperature in Fahrenheit: ");
+    scanf("%lf", &f);
+
+    c = ((f - 32) * 5) / 9;
+
+    printf("Fahrenheit Temperature is: %.2f.", f);
+    printf("\nCelsius Temperature is: %.2f.", c);
+    return 0;
+}
