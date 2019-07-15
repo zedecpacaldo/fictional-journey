4e4b308fa4539a039ab70c62291d83a0a76cfb8e
Zedec Pacaldo <zedec.pacaldo@gmail.com>
2019-07-15 08:18:42 +0800

    Add files via upload

---
+++ Pre-Freshmen Year/main.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+
+    double feet, cm, inch;
+    printf("Enter the distance(feet): ");
+    scanf("%lf", &feet);
+
+    cm = (feet * 12) * 2.54;
+
+    printf("\n%.2f feet = %.2f cm\n", feet, cm);
+    printf("\nEnter distance(inches): ");
+    scanf("%lf", &inch);
+
+    cm = (inch * 2.54);
+
+    printf("\n%.2f inches = %.2f cm", inch, cm);
+}
