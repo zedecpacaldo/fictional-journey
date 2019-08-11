f03d5f9463875aeaf98ec5afeaa0888085ec4f6c
Zedec Pacaldo <zedec.pacaldo@gmail.com>
2019-08-11 21:08:34 +0800

    Update Day in the Week Calculator.c

--- Pre-Freshmen Year/Day in the Week Calculator.c
+++ Pre-Freshmen Year/Day in the Week Calculator.c
@@ -5,8 +5,10 @@ int main()
 {
     int m, d, y, secondPart, formula, centuryCode, actualYear, testYear, finalizer;
     int months[] = {0,1,4,4,0,2,5,0,3,6,1,4,6};
-    char* day[10] = {"Saturday", "Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday"};
-    char* monthNames[13] = {"null", "January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December",};
+    char* day[8] = {"Saturday", "Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday"};
+    char* monthNames[13] = {"null", "January", "February", "March", "April",
+                            "May", "June", "July", "August", "September", "October",
+                            "November", "December",};
 
     printf("Enter Month (MM): ");
     scanf("%d", &m);
@@ -22,32 +24,52 @@ int main()
     if(y > 2050){
         while(y > 2050){
             y = y-400;
-            }}
+            }
+        }
 
-    if(y < 1599 && y > 1500){
+    if(y <= 1599 && y >= 1500){
         centuryCode = 0;
-        } else if(y < 1699){
+        } else if(y <= 1699){
             centuryCode = 6;
-            } else if(y < 1799){
+            } else if(y <= 1799){
                 centuryCode = 4;
-                } else if(y < 1899){
+                } else if(y <= 1899){
                     centuryCode = 2;
-                    } else if(y < 1999){
+                    } else if(y <= 1999){
                         centuryCode = 0;
                         } else if(y >= 2000){
                             centuryCode = 6;
-                        }
+                            }
 
-    testYear = actualYear%4;
+    if(actualYear%4 == 0 || actualYear%100 == 0){
+        if(m == 2){
+            if(d > 29){
+                printf("Invalid Day for the Month.");
+                return 0;
+                }
+            } else if(d > 28){
+                printf("Invalid Day for the Month.");
+                return 0;
+                }
+            } else if(m == 4 || m == 6 || m == 9 || m == 11){
+                    if(d > 30){
+                        printf("Invalid Day for the Month.");
+                        return 0;
+                        }
+                    }
+
     formula = (d + months[m] + centuryCode + secondPart/4 + secondPart);
 
-    if(testYear == 0){
-        formula =  formula - 1;
+    if(actualYear%4 == 0 || actualYear%100 == 0){
+            if(m < 3){
+                formula =  formula - 1;
+            }
     } else {
         formula = formula;
         }
 
     finalizer = formula%7;
+    /*printf("\n%d + %d + %d + %d + %d + %d", d, months[m], centuryCode, secondPart/4, secondPart, formula);*/
     printf("\n%s %d, %d is a %s.", monthNames[m], d, actualYear, day[finalizer]);
     return 0;
 }
