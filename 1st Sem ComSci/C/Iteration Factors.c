97bfc93f46707ca22f6fdfc037edec4ec27d45b5
Zedec Pacaldo <zedec.pacaldo@gmail.com>
2019-08-27 14:25:40 +0800

    Update Iteration Factors.c

--- 1st Sem ComSci/C/Iteration Factors.c
+++ 1st Sem ComSci/C/Iteration Factors.c
@@ -8,24 +8,12 @@ int main()
     printf("Input M: "); scanf("%d", &m);
     printf("Input N: "); scanf("%d", &n);
 
-    if(m>n)
-    {
-        for(i=1; i<=m; i++)
+        for(i=1; i<=n; i++)
         {
             if(m%i==0 && n%i==0)
             {
                 printf("%d\n", i);
             }
         }
-    } else
-    {
-        for(i=1; i<n; i++)
-        {
-            if(m%i==0 && n%i==0)
-            {
-                printf("%d\n", i);
-            }
-        }
-    }
     return 0;
 }
