5283855b00e9d3e680f16c7cea021247aa9b9e27
Zedec Pacaldo <zedec.pacaldo@gmail.com>
2019-10-04 13:25:24 +0800

    Create Concatenate.c

---
+++ 1st Sem ComSci/C/Concatenate.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main(){
+    char word1[10] = {};
+    char word2[10] = {};
+
+    scanf("%s", word1); 
+    scanf("%s", word2);
+
+    printf("%s", word1); 
+    printf("%s", word2);
+
+}
